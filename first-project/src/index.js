8f8e3b859635e15dec262affcde2d55b4541fc6c
Vinicius Silva <vinilrsilv@gmail.com>
2022-09-29 11:17:47 -0300

    [First Project]: First Component

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom";
 import React from 'react';
 import './index.css';
 
+import First from './components/base/First';
+
 ReactDOM.render(
         <h1>
-            Hello there
+            <First></First>
+            <First></First>
         </h1>,
         document.getElementById("root")
     );
