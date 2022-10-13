298f325af8039aad60865dbfb8cd8610c7218c8c
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-13 19:37:38 -0300

    [Calculator]: Applying events to each button

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import './Button.css';
 
 export default props => {
+
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
     return (
-        <button className="button">{props.label}</button>
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+                {props.label}
+            </button>
     )
 }
