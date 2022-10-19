d5266403823db6d1f5d37ca1d620b191b0634adc
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-19 08:57:51 -0300

    [Hooks]: Using setState()

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,10 +1,15 @@
 import './index.css'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import React from 'react'
 
 import App from './views/App'
 
-ReactDOM.render(
-    <App />,
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// )
+
+const root = ReactDOM.createRoot(
     document.getElementById('root')
-)
+);
+root.render(<App />)
--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = (props) => {
+    let [count, setCount] = useState(0);
+    let [name, setName] = useState('Peter')
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercise #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={() => setCount(0)}>0</button>
+                    <button className="btn" onClick={() => setCount(count - 1)}>-1</button>
+                    <button className="btn" onClick={() => setCount(count + 1)}>+1</button>
+                    <button className="btn" onClick={() => setCount(current => current + 1000)}>+ 1000</button>
+                </div>
+            </div>
+            <SectionTitle title="Exercise #02" />
+            <input type="text" className="input" value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
     )
 }
