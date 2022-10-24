f32a571e6b3f8c25b9e46822859a3fdd7259a97f
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-24 15:51:08 -0300

    [Hooks]: Using useRef #02

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -5,11 +5,27 @@ import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(s1, s2) {
+    return [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("")
+}
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
-    useEffect(function () { count.current += 1 }, [value1])
+    console.log(value2[value2.length -1])
+
+    useEffect(function () {
+        count.current++
+        myInput2.current.focus()
+    }, [value1])
+    useEffect(function () {
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
 
     return (
         <div className="UseRef">
@@ -22,15 +38,26 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Value: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1, value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>
                 <input
                     type="text"
                     className="input"
+                    ref={myInput1}
                     value={value1}
                     onChange={(e) => setValue1(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+                <input
+                    type="text"
+                    className="input"
+                    ref={myInput2}
+                    value={value2}
+                    onChange={(e) => setValue2(e.target.value)} />
             </div>
         </div>
     )
