d3ed2c107bd635fdf06382d30a87e8bd273ecf8c
Vinicius Silva <vinilrsilv@gmail.com>
2022-11-07 07:31:21 -0300

    [DB Integration]: Modularized CSS

---
+++ nextjs-class/first-principles/src/pages/stylish.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Stylish.module.css';
+
+export default function Stylish() {
+
+    return (
+        <div className={styles.purple}>
+            <h1>Style using CSS Modules</h1>
+        </div>
+    )
+}
