034a38a15616e7d2f300b68aca2bff228a05d1ab
Vinicius Silva <vinilrsilv@gmail.com>
2022-11-08 07:37:40 -0300

    [DB Integration]: Using a component

---
+++ nextjs-class/first-principles/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Principles of Next.js & React</h1>
+        </header>
+    )
+}
---
+++ nextjs-class/first-principles/src/pages/example.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function Exemple() {
+    return (
+        <>
+            <Header />
+            <h1>Example</h1>
+        </>
+    )
+}
