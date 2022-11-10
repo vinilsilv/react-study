3cf6d9e348cea93e54791e1fee57d6e41d2df504
Vinicius Silva <vinilrsilv@gmail.com>
2022-11-10 07:17:54 -0300

    [DB Integration]: Navigation between components

--- nextjs-class/first-principles/src/pages/stylish.jsx
+++ nextjs-class/first-principles/src/pages/stylish.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import styles from '../styles/Stylish.module.css';
 
 export default function Stylish() {
 
     return (
         <div className={styles.purple}>
+            <Link href="/">
+                <h1>Home</h1>
+            </Link>
             <h1>Style using CSS Modules</h1>
         </div>
     )
