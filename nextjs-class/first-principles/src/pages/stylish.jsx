818b35255e3289f87c5b69149e8536e1b7e6e6b5
Vinicius Silva <vinilrsilv@gmail.com>
2022-11-11 07:59:11 -0300

    [DB Integration]: Adding a 'Layout' component

--- nextjs-class/first-principles/src/pages/stylish.jsx
+++ nextjs-class/first-principles/src/pages/stylish.jsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
+import Layout from '../components/Layout';
 import styles from '../styles/Stylish.module.css';
 
 export default function Stylish() {
 
     return (
-        <div className={styles.purple}>
-            <Link href="/">
-                <h1>Home</h1>
-            </Link>
-            <h1>Style using CSS Modules</h1>
-        </div>
+        <Layout title="CSS Modularized example">
+            <div className={styles.purple}>
+                <h1>Style using CSS Modules</h1>
+            </div>
+        </Layout>
     )
 }
