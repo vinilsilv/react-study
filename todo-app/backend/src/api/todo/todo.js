c781e988e0b85bb3735aa96808011092e00a03ea
Vinicius Silva <vinilrsilv@gmail.com>
2022-11-26 08:36:58 -0300

    [Todo App][BackEnd]: ODM and creating the API REST

---
+++ todo-app/backend/src/api/todo/todo.js
@@ -0,0 +1,10 @@
+const resful = require('node-restful')
+const mongoose = restful.mongoose
+
+const todoSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  done: { type: Boolean, required: true, default: false },
+  createdAt: { type: Date, default: Date.now }
+})
+
+module.exports = resful.model('Todo', todoSchema)
