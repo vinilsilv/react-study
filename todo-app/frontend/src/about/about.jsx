7594efb2d2ad432f2debfb7bc9b516d17ca140e9
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-04 15:24:45 -0300

    [Todo App][FrontEnd]: PageHEader component

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
   <div>
-    <h1>About</h1>
+    <PageHeader name='About' small='Us'/>
+
+    <h2>Our story</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Our values</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <p>Lorem ipsum dolor sit amet...</p>
   </div>
 )
