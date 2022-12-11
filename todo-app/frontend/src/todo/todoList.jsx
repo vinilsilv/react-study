8cb2f7ed98c5dba90e80500119aa98abd4acd24e
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-10 21:29:41 -0300

    [Todo App][FrontEnd]: Mark as read/pending

--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import IconButton from "../template/iconButton";
 
+
 export default props => {
 
   const renderRows = () => {
@@ -8,9 +9,16 @@ export default props => {
 
     return list.map(todo => (
       <tr key={todo._id}>
-        <td>{todo.description}</td>
-        
-        <IconButton style='danger' icon='trash' onClick={() => props.handleRemove(todo)}></IconButton>
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+
+        <td>
+          <IconButton style='success' icon='check' hide={todo.done}
+            onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+          <IconButton style='warning' icon='undo' hide={!todo.done}
+            onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+          <IconButton style='danger' icon='trash' hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}></IconButton>
+        </td>
       </tr>
     ))
   }
