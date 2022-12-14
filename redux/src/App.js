656aa0ad6e267fdf438d572a459ce7df7c01f6f1
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-14 05:36:38 -0300

    [Redux]: Improving Card component styling

--- redux/src/App.js
+++ redux/src/App.js
@@ -7,16 +7,12 @@ function App() {
     <div className="App">
       <h1>React Redux</h1>
       <div className='line'>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 1" red>X</Card>
+        <Card title="Card 2" green>Y</Card>
       </div>
       <div className='line'>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>Y</Card>
       </div>
     </div>
   );
