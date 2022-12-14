32fd31f08b56f051ce793bce883277826bdb35a2
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-14 05:22:22 -0300

    [Redux]: Card component + styling

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Redux</h1>
+      <div className='line'>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+      <div className='line'>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
     </div>
   );
 }
---
+++ redux/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css'
+import React from 'react'
+
+export default props => {
+  return (
+    <div>
+      Card
+    </div>
+  )
+}
