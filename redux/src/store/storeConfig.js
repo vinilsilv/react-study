a4128c1a014b2b9050a997025b29522f2e0603be
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-16 05:25:15 -0300

    [Redux]: Reducers refactor

--- redux/src/store/storeConfig.js
+++ redux/src/store/storeConfig.js
@@ -1,35 +1,9 @@
 import { legacy_createStore as createStore, combineReducers } from 'redux'
 
+import numbersReducer from './reducers/numbers'
+
 const reducers = combineReducers({
-  numbers: function(state, action) {
-    
-    switch(action.type) {
-      case 'MIN_NUM_CHANGED':
-        return {
-          ...state,
-          min: action.payload
-        }
-      case 'MAX_NUM_CHANGED':
-        return {
-          ...state,
-          max: action.payload
-        }
-      default:
-        return {
-          min: 7,
-          max: 31
-        }
-    }
-  },
-  names: function(state, action) {
-    console.log('Name Reducer')
-    console.log(state, action)
-    return [
-      'John',
-      'Peter',
-      'Charles'
-    ]
-  }
+  numbers: numbersReducer
 })
 
 function storeConfig() {
