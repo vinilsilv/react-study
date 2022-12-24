c71778f1c70793f4e20db2ba7a3bc5c311a09e13
Vinicius Silva <vinilrsilv@gmail.com>
2022-12-24 02:04:56 -0300

    [My Money App]: Setting error messages

--- my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -7,7 +7,7 @@ const creditSchema = new mongoose.Schema({
 })
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: { type: Number, min: 0, required: [true, 'Inform debt value.'] },
   status: { type: String, required: false, uppercase: true,
     enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 })
--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.Number.min = "Informed '{VALUE}' is less than the min value of '{MIN}'."
+mongoose.Error.messages.Number.max = "Informed '{VALUE}' is greater than the max value of '{MAX}'."
+mongoose.Error.messages.Number.enum = "'{VALUE}' is not valid for the '{PATH}' attribute."
