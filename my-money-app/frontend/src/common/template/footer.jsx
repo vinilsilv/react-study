c681650d9c3217fd4648fb40d35635c1c874a383
Vinicius Silva <vinilrsilv@gmail.com>
2023-01-03 22:29:27 -0300

    [My Money App]: Footer component

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2023
+      <a href="https://vinilsilv.com" target='_blank'> Site</a>.
+    </strong>
+  </footer>
+)
