3785bb0e87292d9f345cddbc47385a250a6616be
Vinicius Silva <vinilrsilv@gmail.com>
2023-01-06 19:14:01 -0300

    [My Money App]: Using ValueBox component

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/content-header'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
   render() {
@@ -9,7 +11,14 @@ class Dashboard extends Component {
       <div>
         <ContentHeader title='Dashboard' small='Version 1.0' />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank'
+              value="R$ 10" text="Credit Total" />
+            <ValueBox cols='12 4' color='red' icon='credit-card'
+              value="R$ 10" text="Debit Total" />
+            <ValueBox cols='12 4' color='blue' icon='money'
+              value="R$ 0" text="Consolidated Value" />
+          </Row>
         </Content>
       </div>
     )
