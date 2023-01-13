5927892dbc4068eea98df0b65f37776cbba7cb74
Vinicius Silva <vinilrsilv@gmail.com>
2023-01-12 21:55:04 -0300

    [My Money App]: Creating TabHeader component

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -4,6 +4,7 @@ import ContentHeader from '../common/template/content-header'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
 
 export default class extends Component {
   render() {
@@ -12,7 +13,12 @@ export default class extends Component {
         <ContentHeader title='Payment Cycle' small='Register' />
         <Content>
           <Tabs>
-            <TabsHeader></TabsHeader>
+            <TabsHeader>
+              <TabHeader label="List" icon="bars" target="tabList" />
+              <TabHeader label="Include" icon="plus" target="tabCreate" />
+              <TabHeader label="Edit" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Delete" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
             <TabsContent></TabsContent>
           </Tabs>
           Payment Cycles
