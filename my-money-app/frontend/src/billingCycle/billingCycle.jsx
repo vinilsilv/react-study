0242d9953d9fd1b15572dd74803a4a29e5a53cc1
Vinicius Silva <vinilrsilv@gmail.com>
2023-01-14 22:23:29 -0300

    [My Money App]: TabContent component

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
 
 export default class extends Component {
   render() {
@@ -19,7 +20,21 @@ export default class extends Component {
               <TabHeader label="Edit" icon="pencil" target="tabUpdate" />
               <TabHeader label="Delete" icon="trash-o" target="tabDelete" />
             </TabsHeader>
-            <TabsContent></TabsContent>
+            
+            <TabsContent>
+              <TabContent id='tabList'>
+                <h1>List</h1>
+              </TabContent>
+              <TabContent id='tabCreate'>
+                <h1>Include</h1>
+              </TabContent>
+              <TabContent id='tabUpdate'>
+                <h1>Edit</h1>
+              </TabContent>
+              <TabContent id='tabDelete'>
+                <h1>Delete</h1>
+              </TabContent>
+            </TabsContent>
           </Tabs>
           Payment Cycles
         </Content>
--- my-money-app/frontend/src/common/tab/tabReducer.js
+++ my-money-app/frontend/src/common/tab/tabReducer.js
@@ -3,9 +3,10 @@ const INITIAL_STATE = { selected: '' }
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "TAB_SELECTED":
+      const selected = action.payload
       return {
         ...state,
-        selected: action.payload
+        selected
       }
     default:
       return state
