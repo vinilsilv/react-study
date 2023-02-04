872165267fa4d7971829e396000ba84b7268ad77
Vinicius Silva <vinilrsilv@gmail.com>
2023-02-04 10:13:03 -0300

    [My Money App]: Displaying success and error messages

--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -12,6 +14,14 @@ export function getList() {
 
 export function create(values) {
   axios.post(`${BASE_URL}/billingCycles`, values)
+    .then(() => {
+      toastr.success('Success', 'Operation was successful')
+    })
+    .catch(errors => {
+      errors.response.data.errors.forEach(error => {
+        return toastr.error('Error', error)
+      })
+    })
 
   return {
     type: 'TEMP'
