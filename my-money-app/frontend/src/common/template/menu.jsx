f07d82bcbc4a589b71521f93ae00d3e7f332356b
Vinicius Silva <vinilrsilv@gmail.com>
2023-02-24 09:52:44 -0300

    [My Money App]: Navigation with React-router

--- my-money-app/frontend/src/common/template/menu.jsx
+++ my-money-app/frontend/src/common/template/menu.jsx
@@ -4,9 +4,9 @@ import MenuTree from './menuTree'
 
 export default props => (
   <ul className="sidebar-menu">
-    <MenuItem path='#' label='Dashboard' icon='dashboard' />
+    <MenuItem path='/' label='Dashboard' icon='dashboard' />
     <MenuTree label='Register' icon='edit'>
-      <MenuItem path='#billingCycles' label='Billing Cycles' icon='usd' />
+      <MenuItem path='billingCycles' label='Billing Cycles' icon='usd' />
     </MenuTree>
   </ul>
 )
