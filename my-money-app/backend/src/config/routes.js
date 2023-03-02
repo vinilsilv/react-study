3ff0f0f826cbee841f5d780b935b11eff6ef0a95
Vinicius Silva <vinilrsilv@gmail.com>
2023-03-02 18:45:55 -0300

    [My Money App][Backend]: Updating routes.js

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
-  const router = express.Router()
-  server.use('/api', router)
+  
+  // Closed Routes
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+
+  protectedApi.use(auth)
 
   const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  BillingCycle.require(protectedApi, '/billingCycles')
+
+  // Open Routes
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+
+  const AuthService = require('../api/user/AuthSerivce')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
