50529141bba3e5b7b1f6151c27281423fd87ac49
Vinicius Silva <vinilrsilv@gmail.com>
2023-03-12 21:45:14 -0300

    [Exercises]: Creating and using component with props

---
+++ exercises/src/components/Morning.jsx
@@ -0,0 +1,6 @@
+export default props => (
+  <>
+    <h1>{`Good morning, ${props.name}!`}</h1>
+    <h2>See you soon!</h2>
+  </>
+)
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,8 +1,9 @@
 import * as ReactDOMClient from 'react-dom/client'
 
 import First from './components/First'
+import Morning from './components/Morning'
 
 const rootElement = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<First/>);
+root.render(<Morning name='Joe'/>);
