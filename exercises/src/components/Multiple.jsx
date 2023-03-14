aec69f8c04636c70800972b3f25c186d3cc64b46
Vinicius Silva <vinilrsilv@gmail.com>
2023-03-13 21:54:31 -0300

    [Exercises]: Creating and exporting multiple components from a single file

---
+++ exercises/src/components/Multiple.jsx
@@ -0,0 +1,6 @@
+const Afternoon = props => <h1>Good afternoon, {props.name}!</h1>
+
+const Evening = props => <h1>Good evening, {props.name}!</h1>
+
+export { Afternoon, Evening }
+export default { Afternoon, Evening }
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -2,8 +2,14 @@ import * as ReactDOMClient from 'react-dom/client'
 
 import First from './components/First'
 import Morning from './components/Morning'
+import Multiple, { Evening } from './components/Multiple'
 
 const rootElement = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<Morning name='Joe'/>);
+root.render(
+  <>
+    <Multiple.Afternoon name='Joe' />
+    <Evening name='Joe' />
+  </>
+);
